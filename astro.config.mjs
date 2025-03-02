0b0e5d59c27a30eb64c2fa238e88e220588f9ae3
Jackie <jackieec956@gmail.com>
2025-03-02 04:35:50 +0000

    First website commmit

---
+++ astro.config.mjs
@@ -0,0 +1,19 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import fs from 'fs';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import tailwindcss from '@tailwindcss/vite';
+// https://astro.build/config
+import node from '@astrojs/node';
+export default defineConfig({
+	site: 'https://jackie-courtney.com',
+	export default defineConfig({
+	output: 'server',
+	adapter: cloudflare(),
+    });
+});
+
+
+
+
---
+++ src/content/blog/markdown-style-guide.md
@@ -0,0 +1,9 @@
+---
+title: 'Personal Server Setup'
+description: 'This is how I developed my personal home server setup.'
+pubDate: 'Feb 14 2024'
+heroImage: '/blog-placeholder-1.jpg'
+---
+
+
+Hi! Not done yet :)
